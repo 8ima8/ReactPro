df3faf68ec511d37c412fd3da3356db7a06c4c1c
8ima8 <fa.beltransalas@gmail.com>
2025-04-11 17:10:32 -0400

    Add the Lazy routing function

--- projects/07-router/src/App.jsx
+++ projects/07-router/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
-import React from 'react'
-import  HomePage  from './pages/Home'
-import AboutPage from './pages/About'
+import React, { Suspense } from 'react'
+import { lazy } from 'react'
+//import  HomePage  from './pages/Home'
+//import estatico
 import { Router } from './Router'
 import { Route } from './Route'
 import { Page404 } from './pages/404'
 import { SearchPage } from './pages/Search'
+
+//import dinamico
+const LazyAboutPage= lazy(()=> import ('./pages/About.jsx'))
+const LazyHomePage= lazy(()=> import ('./pages/Home.jsx'))
 
 const routes = [
   {
@@ -22,10 +27,12 @@ function App() {
 
   return (
     <main>
+      <Suspense fallback={<div>Loading...</div>}>
       <Router routes={routes} defaultComponent={Page404}>
-        <Route path='/' Component={HomePage}/>
-        <Route path='/about' Component={AboutPage}/>
+        <Route path='/' Component={LazyHomePage}/>
+        <Route path='/about' Component={LazyAboutPage}/>
       </Router>
+      </Suspense>
     </main>
   )
 }
